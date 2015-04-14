7c0a8060731213375347e179285add86364817f4
ltobler <tobler.lu@bluewin.ch>
2015-04-14 19:35:06 +0200

    primitive region support added

--- plugins/LeagueOfLegends.lua
+++ plugins/LeagueOfLegends.lua
@@ -2,13 +2,13 @@
 --Author: ltobler and sgitkene
 
 -- WORK IN PROGRESS
-
+local region="euw"
 local api_key=""
 local news_amount=5
 
 local function get_id(summoner_name) --TODO
-    local url="https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/"..summoner_name..\
-        "?api_key="..api_key 
+    local url="https://"..region..".api.pvp.net/api/lol/"..region..\
+        "/v1.4/summoner/by-name/"..summoner_name.."?api_key="..api_key 
     local b,status = http.request()
     if status ~= 200 then --200 = OK
         return nil 
@@ -58,6 +58,11 @@ function run(msg, matches)
             return "New news feed amount: "..num
         else
             return "ERROR: Invalid amount"
+        end
+    elseif(matches[1]=="region") then
+        local r = matches[1]
+        if r=="euw" or r=="na" then --TODO add support for all regions
+            region = r   
         end   
     end
 end
@@ -69,13 +74,15 @@ return{
         "!lolstatus [summoner]: Current status of summoner",
         "!lolnews: latest League of Legends Esports news"
         "!lolnews change_amount [amount]: Changes number of returned articles(default is 5, \
-            0 < amount <= 30)"
+            0 < amount <= 30)",
+        "!lol region [region]: Sets region[euw, na](default is euw)"
     },
     patterns={
         "^(!elo) (.+)$",
         "^(!lolstatus) (.+)$",
         "^!lolnews$",
-        "^!lolnews (change_amount) (%w+)$"
+        "^!lolnews (change_amount) (%w+)$",
+        "^!lol (region) (.+)$"
     },
     run = run
 }
